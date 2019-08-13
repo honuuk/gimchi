874aaa71585d797be3cc25176aa84d7725ebe5bb
jewok3617 <jewook3617@gmail.com>
2019-08-13 15:46:36 +0900

    db change to cleardb

--- dbdata.php
+++ dbdata.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password_db = "wp486684";
-$dbname = "web";
+$servername = "us-cdbr-iron-east-02.cleardb.net";
+$username = "b97c2efa975d67";
+$password_db = "ce8b4275";
+$dbname = "heroku_d7d6e81a41b00e6";
 
 ?>
